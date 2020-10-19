3620b38dd4812e6da387bd14c3e13838e12aa357
akira <asa.shakira340@gmail.com>
2020-10-19 12:18:00 +0900

    a

--- 02/b.c
+++ 02/b.c
@@ -6,12 +6,21 @@ signed main(void) {
   printf("<table>\n");
 
   int i, j;
+<<<<<<< HEAD
+  char str;
+  for(i = 0; i < 4; i++) {
+    printf("<tr>");
+    for(j = 0; j < 4; j++) {
+      fscanf(file, "%s", &str);
+      printf("<td>%s</td>", &str);
+=======
   char str[16];
   for(i = 0; i < 4; i++) {
     printf("<tr>");
     for(j = 0; j < 4; j++) {
       fscanf(file, "%s", str);
       printf("<td>%s</td>", str);
+>>>>>>> a8158f15560c73c7f0de2b3ea5f733d4f675e355
     }
     printf("</tr>\n");
   }
